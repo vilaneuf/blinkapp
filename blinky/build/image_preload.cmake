75facfd626c90c87810e506223d9418a5d414fd2
pvilanova <pvilanova@tekniker.es>
2022-05-30 12:12:37 +0200

    submodule nrf

--- blinky/build/image_preload.cmake
+++
@@ -1,6 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf5340dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "C:/nrf_workspace/test_git3/blinky/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "c:\nrf\v1.9.1\toolchain\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
---
+++ blinky/build_1/zephyr/zephyr.dts.d
@@ -0,0 +1,22 @@
+empty_file.o: C:/nrf_workspace/test_git3/zephyr/misc/empty_file.c \
+ C:/nrf_workspace/test_git3/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340dk_nrf5340_cpuapp.dts \
+ C:/nrf_workspace/test_git3/zephyr/dts/arm/nordic/nrf5340_cpuapp_qkaa.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/dts/common/mem.h \
+ C:/nrf_workspace/test_git3/zephyr/dts/arm/nordic/nrf5340_cpuapp.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/dts/arm/armv8-m.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/dts/common/skeleton.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ C:/nrf_workspace/test_git3/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ C:/nrf_workspace/test_git3/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ C:/nrf_workspace/test_git3/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ C:/nrf_workspace/test_git3/zephyr/dts/arm/nordic/override.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/dts/arm/nordic/nrf5340_cpuapp_peripherals.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/dts/arm/nordic/nrf5340_cpuapp_ipc.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/dts/arm/nordic/nrf5340_cpuapp_peripherals_ns.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_cpuapp_common.dts \
+ C:/nrf_workspace/test_git3/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_cpuapp_common-pinctrl.dtsi \
+ C:/nrf_workspace/test_git3/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_cpuapp_partition_conf.dts \
+ C:/nrf_workspace/test_git3/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_shared_sram_planning_conf.dts \
+ C:/nrf_workspace/test_git3/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340dk_nrf5340_cpuapp-pinctrl.dtsi \
+ C:/nrf_workspace/test_git3/blinky/nrf5340dk_nrf5340_cpuapp.overlay
